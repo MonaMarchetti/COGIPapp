f9e91f8a59923ea71361a7e765c802ed359ea8fc
caterinamennito <caterina.mennito@gmail.com>
2019-04-11 09:09:56 +0200

    societe with some problems

--- contacts.php
+++ contacts.php
@@ -4,7 +4,7 @@
 try
 {
 	// On se connecte à MySQL
-	$bdd= new PDO('mysql:host=localhost;dbname=Cogip;charset=utf8', 'user123', 'user123');
+	$bdd= new PDO('mysql:host=localhost; dbname=Cogip;charset=utf8', 'user123', 'user123');
 
 }
 catch(Exception $e)
@@ -17,121 +17,93 @@ $resultat = $bdd->query('SELECT * FROM personne
     LEFT JOIN societe
     ON personne.idsociete = societe.idsociete
 		');
+
+$list = $bdd->query('SELECT * FROM societe');
+
+if(isset($_POST['submit'])){
+  $nom = $_POST['nom'];
+  $prenom = $_POST['prenom'];
+  $tel = $_POST['tel'];
+  $email = $_POST['email'];
+  $idsociete = $_POST['societe'];
+
+  $rq = $bdd -> prepare("INSERT INTO personne (  nom, prenom, email,  tel, idsociete)
+        VALUES ( :nom, :prenom, :email, :tel, :idsociete )");
+    $rq ->execute(array(
+      'nom' => $nom,
+      'prenom' => $prenom,
+      'email' => $email,
+      'tel' => $tel,
+      'idsociete' => $idsociete
+    )) ;
+    header('location:contacts.php'); //permet de rafraichir la page en y renvoyant automatiquement
+}
+
+if(isset($_POST['supPers'])){
+  $sc = $_POST['select'];
+  foreach($sc as $suppr)
+  {
+    $rq = $bdd -> prepare("DELETE FROM personne WHERE idpersonne = ?");
+    $rq->execute(array(
+      $suppr
+    ));
+  }
+  header('location:contacts.php');
+}
+
 ?>
 
 
 <!DOCTYPE HTML>
-<html>
-<body>
+  <html>
+    <body>
+      <form style="margin:10px" method="post" enctype = "multipart/form-data" action="">
+        <table border='1'>
+        	<tr>
+            <th>Check</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Téléphone</th>
+            <th>email</th>
+            <th>société</th>
+          </tr>
 
-<table border='1'>
-	<tr><td>ID</td><td>Nom</td><td>Prénom</td><td>Téléphone</td><td>email</td><td>société</td></tr>
+        	<?php
+        	while ($donnees = $resultat->fetch())
+        	{ ?>
+          	<tr>
+              <td><input type="checkbox" name="select[]" value="<?= $donnees['idpersonne']?>" /></td>
+              <td><?= $donnees['nom']?></td>
+          		<td><?= $donnees['prenom']?></td>
+          		<td><?= $donnees['tel']?></td>
+          		<td><?= $donnees['email']?></td>
+          		<td><?= $donnees['nomsociete']?></td>
+            </tr>
+          <?php } ?>
+        </table>
 
-	<?php
-	while ($donnees = $resultat->fetch())
+        <h1>Ajouter un contact</h1>
+        <p>Nom:</p>
+        <input type="text" name="nom"><br>
+        <p>Prénom:</p>
+        <input type="text" name="prenom"><br>
+        <p>Téléphone:</p>
+        <input type="number" name="tel"><br>
+        <p>Email:</p>
+        <input type="email" name="email"><br>
+        <p>Societe:</p>
+        <select name="societe">
+          <?php
+          foreach ($list as $value) {
+          ?>
+            <option value="<?= $value['idsociete']?>"><?= $value['nomsociete']?></option>
+          <?php
+          }
+          ?>
+        </select><br>
+        <button type="submit" name="submit" value="insert">Ajouter</button>
+					<input type="submit" name="supPers" value="Supprimer" />
+				</form>
 
-	{ ?>
-	<tr>
-			<td><?= $donnees['idpersonne']?></td>
-			<td><?= $donnees['nom']?></td>
-			<td><?= $donnees['prenom']?></td>
-			<td><?= $donnees['tel']?></td>
-			<td><?= $donnees['email']?></td>
-			<td><?= $donnees['nomsociete']?></td>
-			<td><input type='checkbox' name='id[]' value='<?php $donnees['Id']?>'/></td>
-	</tr><br>
-
-	<?php
-}
-?>
-
-<form style="margin:10px" method="post" enctype = "multipart/form-data" action="">
-<p>Ajouter un contact</p>
-id personne: <input type="text" name="idpersonne"><br>
-Nom: <input type="text" name="nom"><br>
-Prénom: <input type="text" name="prenom"><br>
-Téléphone: <input type="number" name="tel"><br>
-Email: <input type="email" name="email"><br>
-id societe: <input type="text" name="idsociete"><br>
-<button type="submit" name="submit" value="submit">Ajouter</button>
-</form>
-
-<?php
-
-if(isset($_POST['submit'])){
-
-$idpersonne = $_POST['idpersonne'];
-$nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$tel = $_POST['tel'];
-$email = $_POST['email'];
-$idsociete = $_POST['idsociete'];
-
-
-$rq = $bdd -> prepare("INSERT INTO personne ( idpersonne, nom, prenom, email,  tel, idsociete)
-			VALUES (:idpersonne, :nom, :prenom, :email, :tel, :idsociete )");
-	$rq ->execute(array(
-
-		'idpersonne' => $idpersonne,
-		'nom' => $nom,
-		'prenom' => $prenom,
-		'email' => $email,
-		'tel' => $tel,
-		'idsociete' => $idsociete
-
-
-	)) ;
-	?>
-
-<tr>
-		<td><?=$idpersonne?></td>
-		<td><?=$nom?></td>
-		<td><?=$prenom?></td>
-		<td><?=$tel?></td>
-		<td><?=$email?></td>
-		<td><?=$idsociete?></td>
-		<td><input type='checkbox' name='delete[]' value='<?php $donnees['idpersonne']?>'/></td>
-
-</tr>
-</table>
-?>
-
-
-
-<?php
-} else {
-	echo "</table>";
-}
-
-?>
-<form action="" method="post">
-		<input type='submit' name='effacer' value='effacer' />
-</form>
-
-<?php
-
-if(isset($_POST['effacer'])){//to run PHP script on submit
-
-$deleteId = $_POST['delete'];
-echo $deleteId;
-
-for($i = 0; $i< sizeof($deleteId); $i++){
- echo $deleteId[$i];
-
-$quer = $bdd->prepare("DELETE FROM personne WHERE idpersonne =:idpersonne");
-$quer->execute(array(
-'idpersonne' => $deleteId[$i]
-));
-
-	header('location:contacts.php');
-
-
-
-}
-}
-
-$resultat->closeCursor();
-?>
-
-</body>
-</html>
+      </body>
+      </html>
---
+++ societe.php
@@ -0,0 +1,104 @@
+
+<?php
+try
+{
+	// On se connecte à MySQL
+	$bdd= new PDO('mysql:host=localhost; dbname=Cogip;charset=utf8', 'user123', 'user123');
+
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+$resultat = $bdd->query('SELECT * FROM societe');
+
+
+if(isset($_POST['submit'])){
+  $nomsociete = $_POST['nomsociete'];
+  $pays = $_POST['pays'];
+  $tva = $_POST['tva'];
+  $type = $_POST['type'];
+
+  $rq = $bdd -> prepare("INSERT INTO societe ( nomsociete, pays, tva, type)
+        VALUES ( :nomsociete, :pays, :tva, :type)");
+    $rq ->execute(array(
+      'nomsociete' => $nomsociete,
+      'pays' => $pays,
+      'tva' => $tva,
+      'type' => $type,
+    )) ;
+    header('location:societe.php'); //permet de rafraichir la page en y renvoyant automatiquement
+}
+
+if(isset($_POST['supSoc'])){
+  $sc = $_POST['select'];
+  foreach($sc as $suppr)
+  {
+    $rq = $bdd -> prepare("DELETE FROM societe WHERE idsociete = ?");
+    $rq->execute(array(
+      $suppr
+    ));
+  }
+  header('location:societe.php');
+}
+
+?>
+
+
+<!DOCTYPE HTML>
+  <html>
+    <body>
+      <form style="margin:10px" method="post" enctype = "multipart/form-data" action="">
+        <table border='1'>
+        	<tr>
+            <th>Check</th>
+            <th>Id</th>
+            <th>Nom</th>
+            <th>Pays</th>
+            <th>N. TVA</th>
+            <th>type</th>
+          </tr>
+
+        	<?php
+        	while ($donnees = $resultat->fetch())
+        	{ ?>
+          	<tr>
+              <td><input type="checkbox" name="select[]" value="<?= $donnees['idsociete']?>" /></td>
+							<td><?= $donnees['idsociete']?></td>
+							<td><?= $donnees['nomsociete']?></td>
+          		<td><?= $donnees['pays']?></td>
+          		<td><?= $donnees['tva']?></td>
+          		<td><?= $donnees['type']?></td>
+            </tr>
+          <?php } ?>
+        </table>
+
+        <h1>Ajouter une société</h1>
+        <p>Nom Société:</p>
+        <input type="text" name="nomsociete"><br>
+        <p>Pays:</p>
+        <input type="text" name="pays"><br>
+        <p>N. TVA: </p>
+        <input type="number" name="tva"><br>
+        <p>type: </p>
+				<select name="type">
+          <?php
+					$list = $bdd->query('SELECT * FROM societe');
+
+          foreach ($list as $value) {
+
+
+          ?>
+            <option value="<?= $value['type']?>"><?= $value['type']?></option>
+          <?php
+          }
+									    ?>
+        </select><br>
+        <button type="submit" name="submit" value="submit">Ajouter</button>
+					<input type="submit" name="supSoc" value="Supprimer" />
+				</form>
+
+      </body>
+      </html>
